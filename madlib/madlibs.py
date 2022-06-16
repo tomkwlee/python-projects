125428eca0d633d25b36dd8116da720599e79019
tomkwlee <tomkwlee06@gmail.com>
2022-06-16 15:02:11 -0700

    madlib

---
+++ madlib/madlibs.py
@@ -0,0 +1,10 @@
+# youtuber = 'Tom Lee'
+
+# print(f'subscribe to {youtuber}')
+adj = input("Adjective: ")
+verb1 = input('Verb: ')
+verb2 = input('Verb: ')
+famous_person = input('Famous person: ')
+madlib = f'computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!'
+
+print(madlib)
